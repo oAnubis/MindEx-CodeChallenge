a045eb0268845e397ac452bb2843fff89807a4ad
oAnubis <bcoburn2018@gmail.com>
2022-09-29 19:04:43 -0400

    Update: CompensationController Post Action Tests Passed.
    To Test the GetCompensationById action, I either need to push to the database then test, or create a simple json seed file to test.
    Opting to create the json (json practice opportunity)

--- CodeChallenge.Tests/CompensationControllerTests.cs
+++ CodeChallenge.Tests/CompensationControllerTests.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using CodeChallenge.Models;
+using CodeCodeChallenge.Tests.Integration.Extensions;
 using CodeCodeChallenge.Tests.Integration.Helpers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeChallenge.Tests.Integration
@@ -15,9 +20,11 @@ namespace CodeChallenge.Tests.Integration
     {
         private static HttpClient _httpClient;
         private static TestServer _testServer;
+        private DbSet<Employee> testSet;
 
         [ClassInitialize]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter",
+            Justification = "<Pending>")]
 
         public static void InitializeClass(TestContext context)
         {
@@ -35,10 +42,43 @@ namespace CodeChallenge.Tests.Integration
         [TestMethod]
         public void CreateCompensation_Returns_Created()
         {
+            var employee = new Employee()
+            {
+                Department = "Complaints",
+                FirstName = "Debbie",
+                LastName = "Downer",
+                Position = "Receiver",
+                EmployeeId = "1234-5678-9876-543-21"
+            };
 
+
+            var compensation = new Compensation()
+            {
+                Employee = employee,
+                Salary = "$82,250",
+                EffectiveDate = "09/29/2022",
+            };
+
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/compensation",
+                new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            var newCompensation = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(newCompensation.CompensationId);
+            Assert.AreEqual(compensation.Salary, newCompensation.Salary);
+            Assert.AreEqual(compensation.EffectiveDate, newCompensation.EffectiveDate);
         }
 
+        [TestMethod]
+        public void GetCompensationById_Returns_Ok()
+        {
+
+        }
     }
-
-
 }
